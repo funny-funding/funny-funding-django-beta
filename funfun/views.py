3f4ea17ad1ea1f78b7b2cfd247dbf3de961fc1e8
haeunNoh06 <okhagrace06@naver.com>
2024-06-24 10:46:09 +0900

    :sparkles: [feat] Category를 enum으로 추가

--- funfun/views.py
+++ funfun/views.py
@@ -1,25 +1,28 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from funfun.models import Item
+from funfun.models import Item, Category
 
 
 # Create your views here.
-class FundingListView(ListView):
+class ItemListView(ListView):
     model = Item
 
-class FundingCreateView(CreateView):
+class ItemCreateView(CreateView):
     model = Item
     fields = '__all__'
     template_name_suffix = '_create'
     success_url = reverse_lazy('funfun:item_list')
 
-class FundingUpdateView(UpdateView):
+class ItemUpdateView(UpdateView):
     model = Item
     fields = '__all__'
     template_name_suffix = '_update'
     success_url = reverse_lazy('funfun:item_list')
 
-class FundingDeleteView(DeleteView):
+class ItemDeleteView(DeleteView):
     model = Item
     success_url = reverse_lazy('funfun:item_list')
+
+class CategoryListView(ListView):
+    model = Category    # Category 테이블 모든 Category 가져와서 -> category_list라는 context에 담아 category_list.html에 넘긴다
