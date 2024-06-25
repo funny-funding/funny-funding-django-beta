ecf708e7573ef499b90fda7261ab9ce93b7c8c66
seonyo <jsy6535@naver.com>
2024-06-25 14:58:57 +0900

    :fire: [Remove] Category Model 코드 제거

--- funfun/urls.py
+++ funfun/urls.py
@@ -5,7 +5,6 @@ from funfun import views
 app_name = 'funfun'
 
 urlpatterns = [
-    path('category/', views.CategoryListView.as_view(), name='category_list'),
     path('', views.ItemListView.as_view(), name='item_list'),
     path('create/', views.ItemCreateView.as_view(), name='item_create'),
     path('update/<int:pk>', views.ItemUpdateView.as_view(), name='item_update'),
--- funfun/views.py
+++ funfun/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from funfun.models import Item, Category
+from funfun.models import Item
 
 
 # Create your views here.
@@ -23,6 +23,3 @@ class ItemUpdateView(UpdateView):
 class ItemDeleteView(DeleteView):
     model = Item
     success_url = reverse_lazy('funfun:item_list')
-
-class CategoryListView(ListView):
-    model = Category    # Category 테이블 모든 Category 가져와서 -> category_list라는 context에 담아 category_list.html에 넘긴다
